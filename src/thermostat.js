72bfcccbae4385c4328a6a7241076aeb803dea0c
Lee Jones <lee@lees-mbp.lan>
2021-03-11 13:46:12 +0000

    added power saving mode

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,11 +5,16 @@ class Thermostat{
         this.MINIMUM_TEMPERATURE = 10;
         this.temperature = 20;
         this.powerSavingMode = true;
+        this.MAX_LIMIT_PSM_ON = 25;
+        this.MAX_LIMIT_PSM_OFF = 32;
     }
     getCurrentTemperature() {
         return this.temperature;
     }
     up() {
+        if (this.isMaximumTemperature()) {
+            return;
+        }
         this.temperature += 1
     }
     down() {
@@ -29,5 +34,11 @@ class Thermostat{
     }
     switchPowerSavingModeOn() {
         this.powerSavingMode = true;
+    }
+    isMaximumTemperature() {
+        if (this.isPowerSavingmodeOn() === false) {
+            return this.temperature === this.MAX_LIMIT_PSM_OFF;
+        }
+        return this.temperature === this.MAX_LIMIT_PSM_ON;
     }
 };
