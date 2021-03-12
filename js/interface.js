50ae916e7eacdf2678c8816bd53caaf13b8bdb39
Lee Jones <lee@Lees-MacBook-Pro.local>
2021-03-12 13:52:10 +0000

    added a weather API

--- js/interface.js
+++ js/interface.js
@@ -34,6 +34,15 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
   };
+
+ $('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+})
+
 });
 
   
